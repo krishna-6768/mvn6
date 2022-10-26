112099ae2222cca17d94c1915864490ff4243fc5
krishna-6768 <115453932+krishna-6768@users.noreply.github.com>
2022-10-26 14:16:14 +0530

    Add files via upload

---
+++ src/main/java/sid-pkg/App.java
@@ -0,0 +1,13 @@
+package sid-pkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
